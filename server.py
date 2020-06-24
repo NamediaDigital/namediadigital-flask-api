74f08bea5e451d028e6bf020f245d2eb695cf9f6
Res Yeg <shaqk702@gmail.com>
2020-06-23 18:28:19 -0700

    Install additional packages, udpate server file

--- server.py
+++ server.py
@@ -1,7 +1,27 @@
 from flask import Flask
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
+posts = []
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+class Posts(Resource):
+    def get(self, postId):
+        for post in posts:
+            if post['id'] == postId:
+                return post
+        return {'post': None}, 404
+
+    def post(self):
+        post = {'id': 1, 'title': 'New article',
+                'description': 'article description'}
+        posts.append(post)
+        return post, 201
+
+
+api.add_resource(Posts, '/posts')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
